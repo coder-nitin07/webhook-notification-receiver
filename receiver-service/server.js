28afc76be48bfd8e6b8828b9e5391092882358b0
coder-nitin07 <nitinthakur87504@gmail.com>
2025-12-16 14:41:24 +0530

    Update code

--- receiver-service/server.js
+++ receiver-service/server.js
@@ -1,20 +1,40 @@
-const express = require('express');
+const express = require("express");
+const crypto = require("crypto");
+require("dotenv").config();
+
 const app = express();
+const PORT = 3000;
 
-// middleware to parse JSON data
-app.use(express.json());
+// IMPORTANT: capture raw body
+app.use(
+  express.json({
+    verify: (req, res, buf) => {
+      req.rawBody = buf.toString();
+    }
+  })
+);
 
-// webhook endpoint
-app.post('/webhook', (req, res)=>{
-    console.log(`Webhook received`);
-    console.log(`Headers : `, req.headers);
-    console.log('Body : ', req.body);
+app.post("/webhook", (req, res) => {
+  const receivedSignature = req.headers["x-signature"];
+  const secret = process.env.WEBHOOK_SECRET;
 
-    // respond immediately
-    res.status(200).json({ received: true });
+  const expectedSignature = crypto
+    .createHmac("sha256", secret)
+    .update(req.rawBody)
+    .digest("hex");
+
+  if (receivedSignature !== expectedSignature) {
+    console.log("❌ Invalid signature");
+    return res.status(401).json({ error: "Invalid signature" });
+  }
+
+  
+  console.log("✅ Webhook verified");
+  console.log("Body:", req.body);
+
+  res.status(200).json({ received: true });
 });
 
-const PORT = 3000;
-app.listen(PORT, ()=>{
-    console.log(`Webhook receiver running on PORT ${ PORT }`);
+app.listen(PORT, () => {
+  console.log(`Webhook receiver running on PORT ${PORT}`);
 });
--- sender-service/sendWebhook.js
+++ sender-service/sendWebhook.js
@@ -1,6 +1,9 @@
 const axios = require("axios");
+const crypto = require('crypto');
+require("dotenv").config();
 
 const webhookURL = "http://localhost:3000/webhook";
+const secret = process.env.WEBHOOK_SECRET;
 
 const event = {
   eventId: "evt_101",
@@ -12,11 +15,18 @@ const event = {
   timestamp: Date.now()
 };
 
+// create signature
+const payload = JSON.stringify(event);
+const signature = crypto.createHmac('sha256', secret)
+                        .update(payload)
+                        .digest('hex');
+
 async function sendWebhook() {
   try {
     const res = await axios.post(webhookURL, event, {
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        "X-Signature": signature
       }
     });
 
